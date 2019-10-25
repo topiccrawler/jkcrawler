ebced745567c2d8d84ffcbd39b8e4a04cf046db5
Courier Kyn <franklin_a_h_p@qq.com>
2019-10-25 14:35:25 +0800

    m.weibo spider v2.0

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # jkcrawler
 
+**2019/10/25 更新：加入微博话题**
+
 **2019/10/24 更新：加入哔哩哔哩相簿**
 
 使用 Scrapy 写成的 JK 爬虫，图片源自哔哩哔哩、Tumblr、Instagram，以及微博、Twitter (待完成)
@@ -11,6 +13,7 @@
 ```shell script
 scrapy crawl api.vc.bilibili -o data/api.vc.bilibili.jsonlines
 scrapy crawl instagram -o data/instagram.jsonlines
+scrapy crawl m.weibo -o data/m.weibo.jsonlines
 scrapy crawl makooooon.tumblr -o data/makooooon.tumblr.jsonlines
 scrapy crawl ryoryo-chan.tumblr -o data/ryoryo-chan.tumblr.jsonlines
 scrapy crawl sscat-xyz.tumblr -o data/sscat-xyz.tumblr.jsonlines
--- jkcrawler/spiders/m_weibo.py
+++ jkcrawler/spiders/m_weibo.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy_splash import SplashRequest
+import json
+from bs4 import BeautifulSoup
 
 
 class MWeiboSpider(scrapy.Spider):
     name = 'm.weibo'
     allowed_domains = ['m.weibo.cn']
-    start_urls = ['https://m.weibo.cn/search?containerid=231522&q=%23%E5%A5%BD%E6%83%B3%E7%9C%8B%E4%BD%A0%E7%A9%BFjk%E7%9A%84%E6%A0%B7%E5%AD%90%23']
-
-    def start_requests(self):
-        for start_url in self.start_urls:
-            yield SplashRequest(start_url, self.parse, args={'wait': 5})
+    start_urls = [
+        *('https://m.weibo.cn/api/container/getIndex?containerid=231522type%3D1%26q%3D%23%E5%A5%BD%E6%83%B3%E7%9C%8B%E4'
+          '%BD%A0%E7%A9%BFjk%E7%9A%84%E6%A0%B7%E5%AD%90%23&page=' + str(i) for i in range(110)),
+        *('https://m.weibo.cn/api/container/getIndex?containerid=100103type%3D1%26q%3D%23%E5%A5%BD%E6%83%B3%E7%9C%8B%E4'
+          '%BD%A0%E7%A9%BF%E5%88%B6%E6%9C%8D%E7%9A%84%E6%A0%B7%E5%AD%90%23&page=' + str(i) for i in range(120)),
+    ]
 
     def parse(self, response):
-        yield {'image_urls': list(
-            map(lambda x: x.replace('orj360', 'large'), response.css('.weibo-media-wraps img::attr("src")').getall()))}
+        result = json.loads(response.text)
+        if result.get('ok'):
+            for card in result['data']['cards']:
+                yield {
+                    'user': card['mblog']['user']['screen_name'],
+                    'text': card['mblog']['longText']['longTextContent'] if card['mblog'].get(
+                        'isLongText') else BeautifulSoup(card['mblog']['text'], 'lxml').get_text(),
+                    'image_urls': [pic['large']['url'] for pic in card['mblog']['pics']],
+                }
