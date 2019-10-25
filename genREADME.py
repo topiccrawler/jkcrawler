c9b5c521f6336a16b8db24f2a348243ec5eba8c4
Courier Kyn <franklin_a_h_p@qq.com>
2019-10-25 15:01:34 +0800

    update README.md

--- genREADME.py
+++ genREADME.py
@@ -5,11 +5,15 @@ def genREADME():
     head = [
         '# jkcrawler',
         '',
-        '使用 Scrapy 写成的 JK 爬虫，图片源自哔哩哔哩、Tumblr、Instagram，以及微博、Twitter (待完成)',
+        '使用 Scrapy 写成的 JK 爬虫，图片源自哔哩哔哩、Tumblr、Instagram，以及微博、Twitter',
         '',
-        '启动爬虫：',
+        '## 安装依赖',
         '',
-        '在 Windows 上，需要在 PowerShell 中执行以下命令',
+        '`pip install -r requirements.txt`',
+        '',
+        '## 启动爬虫',
+        '',
+        '*在 Windows 上需要在 PowerShell 中执行以下命令*',
         '',
         '```shell script',
     ]
@@ -32,6 +36,8 @@ def genREADME():
         '```',
         '',
         '若要在下一次启动爬虫时恢复工作进度，则需要在命令后面加上 `-s JOBDIR=crawls/{spider_name}`',
+        '',
+        '下载的图片在 `data/full`，相关信息在 `{spider_name}.jsonlines` 里',
         '',
     ]
 
